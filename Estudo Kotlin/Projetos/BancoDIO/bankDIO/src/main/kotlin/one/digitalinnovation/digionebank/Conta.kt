586c1a293bc1a6622cb2cd4fe0350ce676770c35
GuiMonteiroDev <guimonteiro.rodrigues@gmail.com>
2021-08-28 04:18:58 -0300

    alteracao Pessoa -> constructor line 9 criacao classe conta com seus membros, criacao function deposito e saque, ainda nao utilizados

--- Estudo Kotlin/Projetos/BancoDIO/bankDIO/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
+++ Estudo Kotlin/Projetos/BancoDIO/bankDIO/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -1,4 +1,15 @@
 package one.digitalinnovation.digionebank
 
+import java.math.BigDecimal
+
 class Conta {
+    val agencia:String = "1453",
+    val contacorr:String = "146635",
+    val digito:String = "x",
+
+    val saldo:BigDecimal
 }
+
+fun deposito(valor:BigDecimal){}
+
+fun saque(valor:BigDecimal){}
